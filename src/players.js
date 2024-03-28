80b6d9f77aa5d645503fc119dacc90bec9619ba1
Easy64 <yusufmuheez50@gmail.com>
2024-03-28 13:34:57 +0100

    playercards commint

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+    {
+        name: "Rodrygo Goes",
+        team: "Real Madrid",
+        nationality: "Brazil",
+        jerseyNumber: 23,
+        age: "24 years",
+        imageUrl: "https://cdn.vox-cdn.com/thumbor/IHUeDWzFBACnx-qSA48cx6YPais=/0x0:4589x3060/920x613/filters:focal(2394x133:3128x867):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/72628259/1653679591.0.jpg",
+    },
+    {
+        name: "Arde Guler",
+        team: "Real Madrid",
+        nationality: "Turkey",
+        jerseyNumber: 21,
+        age:  "18 years",
+        imageUrl: "https://cdn.vox-cdn.com/thumbor/ZuwPAL7bY4dQ9raehXIkUJlnOmM=/0x0:2019x3028/920x613/filters:focal(817x499:1139x821):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/72392787/1499935607.0.jpg",
+    },
+    {
+        name: "Jude Bellingham",
+        team: "Real Madrid",
+        nationality: "England",
+        jerseyNumber: 10,
+        age:  "20 years",
+        imageUrl: "https://a-sports.tv/wp-content/uploads/2023/10/jude.jpg",
+    },
+    {
+        name: "Toni Kroos",
+        team: "Real Madrid",
+        nationality: "Germany",
+        jerseyNumber: 8,
+        age:  "33 years",
+        imageUrl: "https://www.getfootballnewsgermany.com/assets/germany-v-netherlands-international-friendly-1-scaled.jpg",
+    },
+    ];
+    
+    export default players;
